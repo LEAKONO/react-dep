60a741fc97ce7fc43b0aa544c60f73f7cb047444
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-02 13:36:07 +0300

    feat(TaskList): render tasks with AddTask component

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Menu } from '@headlessui/react';
+import useTasks from '../hooks/useTasks';
+import TaskItem from './TaskItem';
+import AddTask from './AddTask';
+
+export default function TaskList() {
+  const { tasks, addTask, deleteTask, toggleTask } = useTasks();
+
+  return (
+    <div>
+      <AddTask onAdd={addTask} />
+      <motion.ul className="mt-4 space-y-2">
+        {tasks.map((task) => (
+          <TaskItem 
+            key={task.id} 
+            task={task} 
+            onDelete={deleteTask} 
+            onToggle={toggleTask} 
+          />
+        ))}
+      </motion.ul>
+    </div>
+  );
+}
